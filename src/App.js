cd261964775afe77a58f2a40125d608328d6140a
linH2407 <linhlinh24072000@gmail.com>
2022-03-23 15:11:25 +0700

    feat: add content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
 import Search from "./components/molecules/search/Search";
+import Banner from "./components/organisms/Banner";
 import CardGame from "./components/organisms/cards/CardGame";
+import CardList from "./components/organisms/cards/CardList";
 import CardLive from "./components/organisms/cards/CardLive";
 import CardMenu from "./components/organisms/cards/CardMenu";
+import CardReg from "./components/organisms/cards/CardReg";
 import CardSport from "./components/organisms/cards/CardSport";
 import CardTv from "./components/organisms/cards/CardTv";
 import Footer from "./components/organisms/Footer";
@@ -16,23 +20,28 @@ function App() {
       </div>
       <div className="flex mt-[100px] justify-center">
         <div className="w-[20%] pl-10 pr-8">
-          <Search/>
-          <CardSport/>
-          <CardGame/>
-          <CardTv/>
-          <CardLive/>
-          <CardMenu/>
+          <Search />
+          <CardSport />
+          <CardGame />
+          <CardTv />
+          <CardLive />
+          <CardMenu />
         </div>
-        <div className="w-[80%] bg-[#222327] rounded-tl-3xl mb-9">
-          <div className="p-10">
-              <div>
-                <Slider/>
-              </div>
+        <div className="w-[80%] bg-[#222327] rounded-tl-3xl mb-9 px-10 py-9">
+          <div className="flex">
+            <div className="w-[80%] pr-9">
+              <Slider />
+            </div>
+            <div className="w-[20%]">
+              <CardReg />
+              <CardList />
+              <Banner />
+            </div>
           </div>
         </div>
       </div>
       <div>
-        <Footer/>
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/molecules/buttons/PlaceBet.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PlaceBet = () => {
+  return (
+    <div>
+      <button
+        type="button"
+        className="text-black bg-yellow-400 shadow-lg shadow-black-600"
+      >
+        PLACE BET
+      </button>
+    </div>
+  );
+};
+
+export default PlaceBet;
