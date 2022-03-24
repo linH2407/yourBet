6c8bf2566d026c1e687474d818212241f7e752e9
linH2407 <linhlinh24072000@gmail.com>
2022-03-24 17:57:03 +0700

    style: responsive navbar + footer

--- src/components/molecules/search/Search.jsx
+++ src/components/molecules/search/Search.jsx
@@ -11,7 +11,7 @@ const Search = () => {
           name="search"
         />
         <span class="sr-only">Search</span>
-        <span class="absolute inset-y-0 right-0 flex items-center pl-2 pt-2 bg-yellow-400 rounded-full border-black ">
+        <span class="absolute inset-y-0 right-0 flex items-center pt-2 pl-2 bg-yellow-400 rounded-full border-black ">
           <svg class="h-7 w-7 fill-black-200" viewBox="0 0 20 20">
             <path d="M11.407,10.421,8.818,7.832a4.276,4.276,0,1,0-.985.985l2.589,2.589a.7.7,0,0,0,.985-.985ZM2.355,5.352a3,3,0,1,1,3,3,3,3,0,0,1-3-3Z"></path>
           </svg>
